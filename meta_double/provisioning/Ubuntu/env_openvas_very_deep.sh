adea09e666559bf175280105df93acc08de420eb
Idefixus <robinwuerz@web.de>
2020-06-11 22:02:44 +0200

    Added new scan types and a new scanner

---
+++ meta_double/provisioning/Ubuntu/env_openvas_very_deep.sh
@@ -0,0 +1,8 @@
+echo "Doing a openvas basic scan"
+
+# Timer needed because of startup time of demon
+sleep 1m
+
+sudo bash openvas_scan_automation_start_deep.sh $env:IP_VULNERABLE
+#sudo bash openvas_scan_automation_start.sh 192.168.178.24
+echo "Scan finished"
